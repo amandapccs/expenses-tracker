de12dbca445c709a952ce38761c700040ceab02c
Amanda Soares <amandchen@hotmail.com>
2022-05-18 18:34:58 -0300

    Adiciona requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,18 +10,45 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email } = this.props;
+    const { email, currencies } = this.props;
     return (
       <div>
         <h3 data-testid="email-field">{ email }</h3>
         <p data-testid="total-field">0</p>
         <p data-testid="header-currency-field">BRL</p>
+
+        <form>
+          <input type="number" data-testid="value-input" placeholder="Valor da Despesa" />
+
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              { currencies.map((curr) => <option key={ curr }>{ curr }</option>) }
+            </select>
+          </label>
+
+          <select data-testid="method-input">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+
+          <select data-testid="tag-input">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+          <input type="text" data-testid="description-input" placeholder="Descrição" />
+        </form>
       </div>);
   }
 }
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -30,6 +57,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 Wallet.propTypes = {
   email: propTypes.string.isRequired,
+  currencies: propTypes.arrayOf(propTypes.any).isRequired,
   fetchCurrency: propTypes.func.isRequired,
 };
 
