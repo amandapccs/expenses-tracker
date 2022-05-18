608941bc2780074aac9ea4ce2a89c199c57389e4
Amanda Soares <amandchen@hotmail.com>
2022-05-18 17:28:41 -0300

    Adiciona requisitos 2 e 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,25 @@
+import propTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <h3 data-testid="email-field">{ email }</h3>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: propTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
