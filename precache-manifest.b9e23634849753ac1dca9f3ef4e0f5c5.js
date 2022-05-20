dc0b77992ef31a01d76e621f4ce772985ad7ed0e
Amanda Soares <amandchen@hotmail.com>
2022-05-20 16:33:12 -0300

    Updates

---
+++ precache-manifest.b9e23634849753ac1dca9f3ef4e0f5c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0dd317ea103ce287f548d3e73b8df2",
+    "url": "/expenses-tracker/index.html"
+  },
+  {
+    "revision": "1adfdf1633b2da8e3496",
+    "url": "/expenses-tracker/static/css/main.be9f321f.chunk.css"
+  },
+  {
+    "revision": "b4ff31bf0baf984fb511",
+    "url": "/expenses-tracker/static/js/2.8823f3fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expenses-tracker/static/js/2.8823f3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adfdf1633b2da8e3496",
+    "url": "/expenses-tracker/static/js/main.99a78caa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/expenses-tracker/static/js/main.99a78caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb267181a72e0f08b15",
+    "url": "/expenses-tracker/static/js/runtime-main.65647e47.js"
+  },
+  {
+    "revision": "dee3998ed439e1ad87c6bc28f1a6f7fc",
+    "url": "/expenses-tracker/static/media/login-image.dee3998e.svg"
+  },
+  {
+    "revision": "11df69c52a5fc709140c2a4425e8ad88",
+    "url": "/expenses-tracker/static/media/payments_FILL0_wght400_GRAD0_opsz48.11df69c5.svg"
+  }
+]);
